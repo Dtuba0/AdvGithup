ae202bb8ceb9babfce6d829db929a7f2fd0df8d0
tuba dagcı <dagcituba.3599@gmail.com>
2024-10-30 11:44:02 +0300

    v2

--- src/Runner.java
+++ src/Runner.java
@@ -2,5 +2,8 @@ public class Runner {
 
     public static void main(String[] args) {
         System.out.println("Hello word!");
+
+        //version 2
+        System.out.println("versıon 2");
     }
 }
